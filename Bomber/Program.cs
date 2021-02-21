e1da9f01506964b20d6907c20e4383de4ef959af
Andrii Miliuchenko <andrii2003@gmail.com>
2021-02-21 14:33:28 +0300

    add

---
+++ Bomber/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace Bomber
+{
+    class Program
+    {
+        static void printBoard(string[] board)
+        {
+            Console.WriteLine("-------------");
+            Console.WriteLine("| " + board[1] + " | " + board[2] + " | " + board[3] + " | ");
+            Console.WriteLine("-------------");
+            Console.WriteLine("| " + board[4] + " | " + board[5] + " | " + board[6] + " | " );
+            Console.WriteLine("-------------");
+            Console.WriteLine("| " + board[7] + " | " + board[8] + " | " + board[9] + " | ");
+            Console.WriteLine("-------------");
+
+        }
+        static void Main(string[] args)
+        {
+            bool plaing = true;
+            string[] boardExample = new string[10];
+            Console.WriteLine("Position numbers of bombs");
+            while (plaing)
+            {
+                //boards
+                for (int i = 0; i < boardExample.Length; i++)
+                {
+                    boardExample[i] = Convert.ToString(i);
+                }
+                printBoard(boardExample);
+                string[] board = new string[boardExample.Length];
+                for (int i = 0; i < board.Length; i++)
+                {
+                    board[i] = "X";
+                }
+                string[] boardCopy = new string[boardExample.Length];
+                for (int i = 0; i < board.Length; i++)
+                {
+                    boardCopy[i] = " ";
+                }
+                Console.Write("Bomb number: ");
+                int bombNumber = Convert.ToInt32(Console.ReadLine());
+                printBoard(board);
+                //bomb generator
+                Random rand = new Random();
+                for (int i = 0; i < bombNumber; i++)
+                {
+                    int randNum = rand.Next(1, 10);
+                    if (boardCopy[randNum] == " ") boardCopy[randNum] = "O";
+                    else boardCopy[rand.Next(1, 10)] = "O";
+                }
+                //printBoard(boardCopy);
+                int correctCount = 0;
+                for (int i = 0; i < bombNumber + 1; i++)
+                {
+                    Console.Write("Enter a num: ");
+                    int attempt = Convert.ToInt32(Console.ReadLine());
+                    if (boardCopy[attempt] == "O")
+                    {
+                        board[attempt] = "O";
+                        printBoard(board);
+                        correctCount++;
+                    }
+                    else Console.WriteLine("There isn't any bomb there!");
+                    if (correctCount == bombNumber) break;
+                }
+                for (int i = 0; i < board.Length; i++)
+                {
+                    if (boardCopy[i] == "O") board[i] = "O";
+                }
+                Console.WriteLine("Positions, where bobms were:");
+                printBoard(board);
+                Console.WriteLine("Correct answers: {0}", correctCount);
+                Console.Write("Do you want to continue (t / f): ");
+                string continuePlaying = Console.ReadLine();
+                if (continuePlaying != "t") plaing = false;
+            }
+        }
+    }
+}
