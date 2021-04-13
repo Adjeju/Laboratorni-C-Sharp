82ebcc808ddb23a8dd03dcabe3a093d0b16af764
Andrii Miliuchenko <andrii2003@gmail.com>
2021-04-13 09:50:10 +0300

    add

---
+++ Lab15/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab15
+{
+    class Class1 : Interface1
+    {
+        public int number;
+        public Class1(int num)
+        {
+            this.number = num;
+        }
+        public int getSum()
+        {
+            int tempNUm = number;
+            int sum = 0;
+            while (tempNUm != 0)
+            {
+                int ost = tempNUm % 10;
+                sum += ost;
+                tempNUm = (tempNUm - ost) / 10;
+            }
+            return sum;
+        }
+
+        public int ZeroCount()
+        {
+            int tempNUm = number;
+            int count = 0;
+            while (tempNUm > 0)
+            {
+                int ost = tempNUm % 10;
+                if (ost == 0) count++;
+                tempNUm = (tempNUm - ost) / 10;
+            }
+            return count;
+        }
+    }
+}
---
+++ Lab15/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lab15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 num = new Class1(320);
+            Console.WriteLine(num.getSum());
+            Console.WriteLine(num.ZeroCount());
+        }
+    }
+}
