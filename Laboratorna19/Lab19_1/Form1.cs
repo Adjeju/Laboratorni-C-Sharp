a7d3588bd72e55b7aa3166bbdbf875e1c3ddae04
Andrii Miliuchenko <andrii2003@gmail.com>
2021-05-06 15:53:52 +0300

    add

---
+++ Laboratorna19/Lab19_1/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Lab19_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void відкритиToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader sr = new StreamReader(openFileDialog1.FileName);
+                string[] row = sr.ReadLine().Split();
+                double[] numbers = new double[row.Length];
+                double maxNum = -100000;
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    numbers[i] = Convert.ToDouble(row[i]);
+                }
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    if (numbers[i] < 0 && maxNum < numbers[i]) maxNum = numbers[i];
+                }
+                label2.Text = maxNum.ToString();
+                label1.Visible = true;
+                label2.Visible = true;
+
+            }
+        }
+    }
+}
---
+++ Laboratorna19/Lab19_2/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Lab19_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void відкритиToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader sr = new StreamReader(openFileDialog1.FileName);
+                int rows = Convert.ToInt32(sr.ReadLine());
+                int cols = Convert.ToInt32(sr.ReadLine());
+                string[][] strMatrix = new string[rows][];
+                int[,] matrix = new int[rows, cols];
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    strMatrix[i] = sr.ReadLine().Split();
+                }
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < matrix.GetLength(1); j++)
+                    {
+                        matrix[i, j] = Convert.ToInt32(strMatrix[i][j]);
+                    }
+                }
+                int maxElem = 0;
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < matrix.GetLength(1); j++)
+                    {
+                        if (maxElem < matrix[i, j]) maxElem = matrix[i, j];
+                    }
+                }
+                label1.Text = maxElem.ToString();
+            }
+
+        }
+    }
+}
